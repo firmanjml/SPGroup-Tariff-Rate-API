9f19c1f5fcde3031cae378b592843096162b62cd
Firman Jamal <37127022+firmanjml@users.noreply.github.com>
2019-08-24 18:45:36 +0800

    Added POST request

--- README.md
+++ README.md
@@ -27,24 +27,26 @@ http://localhost:8000
 
 ```json
 {
-   "date":"2019-08-24T08:29:12.313Z",
+   "date":"2019-08-24T10:31:06.149Z",
    "tariff_rate":[
       {
          "description":"electricity tariff",
-         "cost_kWh":"24.22",
-         "cost_kWh_gst":"25.92",
+         "cost_kwh":24.22,
+         "cost_kwh_gst":25.92,
          "wef":"1 Jul - 30 Sep19"
       },
       {
          "description":"gas tariff",
-         "cost_kWh":"19.10",
-         "cost_kWh_gst":"20.44",
+         "cost_kwh":19.1,
+         "cost_kwh_gst":20.44,
          "wef":"1 Jul - 30 Sep19"
       },
       {
          "description":"water tariff",
-         "cost_kWh":"$1.21 or $1.52/m³",
-         "cost_kWh_gst":"$1.29 or $1.63/m³ (w GST)",
+         "cost":1.21,
+         "cost_gst":1.29,
+         "cost_cubic_metre":1.52,
+         "cost_cubic_metre_gst":1.63,
          "wef":"<40m³ or > 40m³"
       }
    ]
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-var cors = require('cors')
+const bodyParser = require('body-parser');
+var cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
     let jsonObject = [];
 
     axios({
@@ -15,7 +18,7 @@ app.get('/', async (req, res) => {
     }).then(({ data }) => {
         $ = cheerio.load(data);
         const html_class = $('.col-sm-4.match-height');
-        
+
         html_class.each(function () {
             const t = $(this);
             const paragraph = t.find('p').text();
@@ -23,26 +26,97 @@ app.get('/', async (req, res) => {
             const pArr = paragraph.split('\n');
 
             const description = pArr[1].toLowerCase().substring(0, pArr[1].length - 1);
-            const cost_kWh_gst = pArr[0].replace(" cents/kWh (w GST)", "");
-            const cost_kWh = h2.replace(" cents/kWh", "");
+            const cost_gst = pArr[0].replace(/\$/g, "").replace(" cents/kWh", "").replace(" (w GST)", "").replace("/m³", "");
+            const cost = h2.replace(/\$/g, "").replace(" cents/kWh", "").replace("/m³", "");
             const wef = pArr[2].replace("(", "").replace("wef ", "").replace(")", "");
 
-            jsonObject.push({
-                description,
-                cost_kWh,
-                cost_kWh_gst,
-                wef
-            });
+            if (description === "water tariff") {
+                jsonObject.push({
+                    description,
+                    cost: parseFloat(cost.split(" or ")[0]),
+                    cost_gst: parseFloat(cost_gst.split(" or ")[0]),
+                    cost_cubic_metre: parseFloat(cost.split(" or ")[1]),
+                    cost_cubic_metre_gst: parseFloat(cost_gst.split(" or ")[1]),
+                    wef
+                });
+            } else {
+                jsonObject.push({
+                    description,
+                    cost_kwh: parseFloat(cost),
+                    cost_kwh_gst: parseFloat(cost_gst),
+                    wef
+                });
+            }
+            
         });
         res.json({
             date: new Date(),
             tariff_rate: jsonObject
         });
     });
-    
-    
-    
-})
+});
+
+app.post('/power', function (req, res) {
+    let { power, powerOption, hours } = req.body;
+
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.status(422).json({
+            error: 'Invalid request.'
+        });
+        return;
+    }
+
+    if (powerOption < 0 || powerOption > 1) {
+        res.status(422).json({
+            error: 'Invalid request.'
+        });
+        return;
+    }
+
+    if (hours < 0 || hours > 24) {
+        res.status(422).json({
+            error: 'Hours should be in range of 0 to 24.'
+        });
+        return;
+    }
+
+    axios({
+        url: 'https://www.spgroup.com.sg/what-we-do/billing',
+        method: 'GET'
+    }).then(({ data }) => {
+        $ = cheerio.load(data);
+        const html_class = $('.col-sm-4.match-height');
+        const t = html_class.first()
+        const paragraph = t.find('p').text();
+        const h2 = t.find('h2').text();
+        const pArr = paragraph.split('\n');
+        const description = pArr[1].toLowerCase().substring(0, pArr[1].length - 1);
+        const cost_kWh_gst = pArr[0].replace(" cents/kWh (w GST)", "");
+        const cost_kWh = h2.replace(" cents/kWh", "");
+        const wef = pArr[2].replace("(", "").replace("wef ", "").replace(")", "");
+
+        if (powerOption == 0) {
+            power = power / 1000;
+        }
+
+        const dailyUsage = parseFloat((power * hours) * (cost_kWh / 100)).toFixed(2);
+        const monthlyUsage = parseFloat(dailyUsage * 30).toFixed(2);
+        const yearlyUsage = parseFloat(monthlyUsage * 12).toFixed(2);
+
+        res.json({
+            date: new Date(),
+            description,
+            cost_kwh: parseFloat(cost_kWh),
+            cost_kwh_gst: parseFloat(cost_kWh_gst),
+            wef,
+            result: {
+                daily_usage: parseFloat(dailyUsage),
+                monthly_usage: parseFloat(monthlyUsage),
+                yearly_usage: parseFloat(yearlyUsage)
+            }
+        });
+    });
+});
 
 app.listen(8000, () =>
     console.log(`Backend service running on port 8000!`),
